a96f4790c8b12426121a52259a58c6b3a3c3178e
PeterNgTr <peter.nguyentr@gmail.com>
2021-11-18 12:20:18 +0100

    fix issue

--- main.js
+++ main.js
@@ -1,26 +1,23 @@
 let CryptoJS = require("crypto-js");
 
-let CryptoJSString = {
-    'encrypt': function (value) {
-        let src = JSON.stringify({"api-token": value});
-        const encodedWord = CryptoJS.enc.Utf8.parse(src);
-        return CryptoJS.enc.Base64.stringify(encodedWord);
-    },
+encrypt = function (value) {
+    let src = JSON.stringify({'api-token': value});
+    const encodedWord = CryptoJS.enc.Utf8.parse(src);
+    return CryptoJS.enc.Base64.stringify(encodedWord);
 }
 
 module.exports.templateTags = [{
-    name: 'CryptoJS',
-    displayName: 'CryptoJS',
+    name: 'TamaraCryptoJS',
+    displayName: 'TamaraCryptoJS',
     description: 'Encrypt value using CryptoJS',
     args: [
         {
-            displayName: 'Text to encrypt',
+            displayName: 'Value',
             type: 'string',
             placeholder: '-- enter your text --'
         }
     ],
-    run (value) {
-        value = value || '';
-        return CryptoJSString.encrypt(value);
+    run (context, value) {
+        return encrypt(value);
     }
 }];
